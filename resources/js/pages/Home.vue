0722ce9a47d3bf4943b608be5d02d4220ac7a758
IvanEscalonaPerez <ivan_acua@live.com.mx>
2020-03-11 00:48:17 -0600

    Inicial

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <Header />
+        <v-content>
+            <v-container class="fill-height" fluid>
+                <v-row align="center" justify="center">
+                    <v-col class="text-center">
+                        DASDASKLDASKLDSLAKKSNCKSLNSMDKMKD
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+        <FooterCore />
+    </v-app>
+</template>
+
+<script>
+    import Header from '../components/Header';
+  //import Footer from '../components/Footer';
+    export default {
+        components: {
+            Header,
+            FooterCore: () => import("../components/Footer")
+            }
+    }
+</script>
